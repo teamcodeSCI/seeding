861f5b025ca93d0d7a237c1f27469a3f00b23524
doanduc2000 <doanminhducvip2000@gmail.com>
2023-02-09 11:26:29 +0700

    update

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,30 +1,41 @@
 const url = "https://scigroup.com.vn/cp/seeding/api";
-export const login = async (info) => {
-  try {
-    const response = await fetch(
-      `${url}/get-token?login=${info.login}&password=${info.password}`
-    );
-    const data = await response.json();
-    return data.access_token;
-  } catch (e) {
-    console.log(e);
-  }
-};
-export const getUser = async (token) => {
-  try {
-    const response = await fetch(`${url}/get-name?token=${token}`);
-    const data = await response.json();
-    if (!data) {
-      console.log("data not found");
-      return;
+export const login = async(info) => {
+    try {
+        const response = await fetch(
+            `${url}/get-token?login=${info.login}&password=${info.password}`
+        );
+        const data = await response.json();
+        return data.access_token;
+    } catch (e) {
+        console.log(e);
     }
-    localStorage.setItem(
-      "token",
-      `${token}/${data.data.username}/${data.data.rule}`
-    );
-    return data.data;
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
+};
+export const getUser = async(token) => {
+    try {
+        const response = await fetch(`${url}/get-name?token=${token}`);
+        const data = await response.json();
+        if (!data) {
+            console.log("data not found");
+            return;
+        }
+        localStorage.setItem(
+            "token",
+            `${token}/${data.data.username}/${data.data.rule}`
+        );
+        return data.data;
+    } catch (e) {
+        console.log(e);
+
+    }
+};
+export const updatePassword = async(info) => {
+    try {
+        console.log(info);
+        const response = await fetch(`${url}/update-password?token=${info.token}&password=${info.password}`);
+        const data = await response.json();
+        console.log('5. Update Password', data);
+        return data
+    } catch (e) {
+        console.log(e);
+    }
 };
--- src/components/ChangePass.js
+++ src/components/ChangePass.js
@@ -1,107 +1,106 @@
+import { updatePassword } from "../apis/user.js";
 import { getPage } from "../util/getPage.js";
+import { splitStr } from "../util/splitStr.js";
 import InputGroup from "./InputGroup.js";
 
 export default class ChangePass {
-  constructor({ closeChangePass }) {
-    this.closeChangePass = closeChangePass;
-    this.$container = document.createElement("div");
-    this.$container.className = `modal d-flex align-items-center justify-content-center`;
-    this.$container.style.cssText = `background:rgba(0,0,0,0.5); z-index:100`;
+    constructor({ closeChangePass }) {
+        this.closeChangePass = closeChangePass;
+        this.$container = document.createElement("div");
+        this.$container.className = `modal d-flex align-items-center justify-content-center`;
+        this.$container.style.cssText = `background:rgba(0,0,0,0.5); z-index:100`;
 
-    this.$dialog = document.createElement("div");
-    this.$dialog.className = `modal-dialog`;
-    this.$dialog.style.cssText = `max-width: 350px; width: 90%`;
+        this.$dialog = document.createElement("div");
+        this.$dialog.className = `modal-dialog`;
+        this.$dialog.style.cssText = `max-width: 350px; width: 90%`;
 
-    this.$content = document.createElement("div");
-    this.$content.className = `modal-content`;
+        this.$content = document.createElement("div");
+        this.$content.className = `modal-content`;
 
-    this.$header = document.createElement("div");
-    this.$header.className = `modal-header`;
+        this.$header = document.createElement("div");
+        this.$header.className = `modal-header`;
 
-    this.$title = document.createElement("h5");
-    this.$title.className = `modal-title`;
-    this.$title.innerHTML = `Đổi mật khẩu`;
+        this.$title = document.createElement("h5");
+        this.$title.className = `modal-title`;
+        this.$title.innerHTML = `Đổi mật khẩu`;
 
-    this.$closeBtn = document.createElement("button");
-    this.$closeBtn.className = `btn-close`;
-    this.$closeBtn.addEventListener("click", () => {
-      closeChangePass();
-    });
+        this.$closeBtn = document.createElement("button");
+        this.$closeBtn.className = `btn-close`;
+        this.$closeBtn.addEventListener("click", () => {
+            closeChangePass();
+        });
 
-    this.$body = document.createElement("div");
-    this.$body.className = `modal-body d-flex flex-column p-3 gap-2`;
+        this.$body = document.createElement("div");
+        this.$body.className = `modal-body d-flex flex-column p-3 gap-2`;
 
-    this.$currentPass = new InputGroup({
-      placeholder: "Nhập mật khẩu hiện tại...",
-      type: "password"
-    });
-    this.$newPass = new InputGroup({
-      placeholder: "Nhập mật khẩu mới...",
-      type: "password"
-    });
-    this.$retypePass = new InputGroup({
-      placeholder: "Nhập lại mật khẩu mới...",
-      type: "password"
-    });
 
-    this.$footer = document.createElement("div");
-    this.$footer.className = `modal-footer`;
+        this.$newPass = new InputGroup({
+            placeholder: "Nhập mật khẩu mới...",
+            type: "password"
+        });
+        this.$retypePass = new InputGroup({
+            placeholder: "Nhập lại mật khẩu mới...",
+            type: "password"
+        });
 
-    this.$submitBtn = document.createElement("button");
-    this.$submitBtn.className = `btn btn-primary`;
-    this.$submitBtn.innerHTML = `Đổi mật khẩu`;
-    this.$submitBtn.addEventListener("click", () => {
-      this.changePass();
-    });
+        this.$footer = document.createElement("div");
+        this.$footer.className = `modal-footer`;
 
-    this.$noteBox = document.createElement("div");
-    this.$note = document.createElement("p");
-    this.$note.className = `text-center m-0`;
-    this.$note.style.cssText = `color:red; font-style:italic;font-size:14px`;
-  }
-  showNote = (note) => {
-    this.$note.innerHTML = "";
-    this.$note.innerHTML = note;
-    this.$noteBox.appendChild(this.$note);
-  };
+        this.$submitBtn = document.createElement("button");
+        this.$submitBtn.className = `btn btn-primary`;
+        this.$submitBtn.innerHTML = `Đổi mật khẩu`;
+        this.$submitBtn.addEventListener("click", () => {
+            this.changePass();
+        });
 
-  changePass = () => {
-    if (
-      this.$currentPass.getValue().value === "" ||
-      this.$newPass.getValue().value === "" ||
-      this.$retypePass.getValue().value === ""
-    ) {
-      this.showNote("Vui lòng nhập đủ thông tin !");
-      return;
+        this.$noteBox = document.createElement("div");
+        this.$note = document.createElement("p");
+        this.$note.className = `text-center m-0`;
+        this.$note.style.cssText = `color:red; font-style:italic;font-size:14px`;
     }
-    if (this.$newPass.getValue().value !== this.$retypePass.getValue().value) {
-      this.showNote("Nhập lại mật khẩu không đúng!");
-      return;
+    showNote = (note) => {
+        this.$note.innerHTML = "";
+        this.$note.innerHTML = note;
+        this.$noteBox.appendChild(this.$note);
+    };
+
+    changePass = async() => {
+        if (
+            this.$newPass.getValue().value === "" ||
+            this.$retypePass.getValue().value === ""
+        ) {
+            this.showNote("Vui lòng nhập đủ thông tin !");
+            return;
+        }
+        if (this.$newPass.getValue().value !== this.$retypePass.getValue().value) {
+            this.showNote("Mật khẩu không khớp!");
+            return;
+        }
+        await updatePassword({ token: splitStr(localStorage.getItem("token")).token, password: this.$newPass.getValue().value })
+        this.showNote("Đổi mật khẩu thành công !");
+        setTimeout(() => {
+            this.closeChangePass();
+            localStorage.removeItem("token");
+            getPage();
+        }, 1500);
+    };
+    render() {
+        this.$container.appendChild(this.$dialog);
+        this.$dialog.appendChild(this.$content);
+
+        this.$content.appendChild(this.$header);
+        this.$content.appendChild(this.$body);
+        this.$content.appendChild(this.$footer);
+
+        this.$header.appendChild(this.$title);
+        this.$header.appendChild(this.$closeBtn);
+
+
+        this.$body.appendChild(this.$newPass.render());
+        this.$body.appendChild(this.$retypePass.render());
+        this.$body.appendChild(this.$noteBox);
+
+        this.$footer.appendChild(this.$submitBtn);
+        return this.$container;
     }
-    this.showNote("Đổi mật khẩu thành công !");
-    setTimeout(() => {
-      this.closeChangePass();
-      localStorage.removeItem("token");
-      getPage();
-    }, 1500);
-  };
-  render() {
-    this.$container.appendChild(this.$dialog);
-    this.$dialog.appendChild(this.$content);
-
-    this.$content.appendChild(this.$header);
-    this.$content.appendChild(this.$body);
-    this.$content.appendChild(this.$footer);
-
-    this.$header.appendChild(this.$title);
-    this.$header.appendChild(this.$closeBtn);
-
-    this.$body.appendChild(this.$currentPass.render());
-    this.$body.appendChild(this.$newPass.render());
-    this.$body.appendChild(this.$retypePass.render());
-    this.$body.appendChild(this.$noteBox);
-
-    this.$footer.appendChild(this.$submitBtn);
-    return this.$container;
-  }
 }
